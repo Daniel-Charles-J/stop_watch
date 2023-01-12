aa835c395094f966d9d64046e5092a2d5ab28762
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2023-01-12 18:53:08 +0530

    for user friendly

--- src/Component/DisplayComponent.js
+++ src/Component/DisplayComponent.js
@@ -3,16 +3,16 @@ function DisplayComponent(props){
     return(
         <div className="big_container">
             <div className="containers">
-            <h1>{hr}</h1>
+               <h1>{hr}h</h1>
             </div>
             <div className="containers">
-            <h1>{m}</h1>
+               <h1>{m}m</h1>
             </div>
             <div className="containers">
-            <h1>{s}</h1>
+               <h1>{s}s</h1>
             </div>
             <div className="containers">
-            <h1>{ms}</h1>
+               <h1>{ms}</h1>
             </div>
         </div>
     )
